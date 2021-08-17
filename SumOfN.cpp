360a9c7e586d0b608ae0236cbcc819d3f1f787b6
Sanjib-123 <84591644+Sanjib-123@users.noreply.github.com>
2021-08-17 11:55:13 +0530

    Create SumOfN.cpp

---
+++ SumOfN.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// using namespace std;
+    
+// int main()
+// {
+// 	int n,i,sum=0;
+// 	cout<<"Enter the Number";
+// 	cin>>n;
+	    
+// 	for(i=1;i<=n;i++)
+// 	{
+// 	    sum=sum+i;
+// 	}
+// 	cout<<"Sum of Natural no is"<<sum<<endl;
+	    
+// 	return 0;
+	    
+// }
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i=1,sum=0;
+	cout<<"Enter the Number";
+	cin>>n;
+	    
+	while(i<=n)
+	{
+	    sum=sum+i;
+	    i++;
+	}
+	cout<<"Sum of Natural no is"<<sum<<endl;
+	    
+    return 0;
+    	
+}
